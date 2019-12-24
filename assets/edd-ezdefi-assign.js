7c1e8bd9dd8e591e72985df196250e32e756f9af
Tien Nguyen <mrtienhp97@gmail.com>
2019-12-24 10:25:11 +0700

    fix navigation on exception page

--- assets/edd-ezdefi-assign.js
+++ assets/edd-ezdefi-assign.js
@@ -193,7 +193,8 @@ jQuery(function($) {
     EDD_Ezdefi_Assign.prototype.renderHtml = function(data) {
         var self = this;
         if(data.length === 0) {
-            self.$table.append("<tr><td colspan='4'>Not found</td></tr>")
+            self.$table.append("<tr><td colspan='5'>Not found</td></tr>");
+            return;
         }
         for(var i=0;i<data.length;i++) {
             var number = i + 1;
@@ -284,7 +285,14 @@ jQuery(function($) {
     };
 
     EDD_Ezdefi_Assign.prototype.renderPagination = function(data) {
-        this.$nav.find('.displaying-num .number').text(data['total']);
+        var total = data['total'];
+
+        if(total == 0) {
+            this.$nav.hide();
+            return;
+        }
+
+        this.$nav.find('.displaying-num .number').text(total);
 
         var $links = this.$nav.find('.pagination-links');
         var $prev;

23c07cd577f606ee6dc617fb216d0f679a59c644
Tien Nguyen <mrtienhp97@gmail.com>
2020-01-07 22:57:58 +0700

    fix save exception value

--- includes/class-edd-ezdefi-ajax.php
+++ includes/class-edd-ezdefi-ajax.php
@@ -249,7 +249,6 @@ class EDD_Ezdefi_Ajax
 		} else {
 			$value = $payment['value'] / pow( 10, $payment['decimal'] );
 		}
-		$value = rtrim( number_format( $value, 12 ), '0' );
 
 		$data = array(
 			'amount_id' => str_replace( ',', '', $value),
